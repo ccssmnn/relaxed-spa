39cd217b27274cc7688acebe425694f82320e29c
Carl Assmann <assmann@hey.com>
2023-01-08 11:39:43 +0100

    add doctype

--- server.tsx
+++ server.tsx
@@ -15,7 +15,10 @@ function Index() {
       <head>
         <meta charSet="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <title>Relaxed SPA</title>
       </head>
@@ -35,9 +38,11 @@ const appBundle = bundle(
 
 function indexHTMLResponse() {
   return new Response(
-    ReactDOMServer.renderToString(
-      <Index />,
-    ),
+    `<!DOCTYPE html>${
+      ReactDOMServer.renderToString(
+        <Index />,
+      )
+    }`,
     {
       headers: {
         "Content-Type": "text/html",
